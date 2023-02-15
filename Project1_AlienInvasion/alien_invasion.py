be7d4662c71505f0e641ad9fba232c034bd20d53
jonnyjamison <jonny.jamison@outlook.com>
2023-02-15 20:29:45 +0000

    first commit

---
+++ Project1_AlienInvasion/alien_invasion.py
@@ -0,0 +1,85 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship #Ship class from ship.py file 
+#Importing Settings class from settings module file
+#Instead of adding a bunch of settings throughout code, this allows us to access it in one place
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+
+#WITHIN __init__, ASSIGN ALL THE INSTANCES YOU NEED FROM OTHER MODULES (to self)
+    def __init__(self): 
+        """Initialize the game, and create game resources."""
+        pygame.init() #Initialises the background settings required for Pygame
+
+        self.settings = Settings() #Brings Settings attributes into this instance
+        #for e.g, there is screen_height, screen_width in Settings Module. 
+
+        #Using Settings attributes, we create a screen (using those settings)
+        #Create the screen here, then update it using method below
+        #pygame.display.setmode is from pygame library
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+            self.settings.screen_height)) 
+
+        #Notice how the above is called - SELF.SETTINGS.screen_height
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self) #creating an instance of Ship (Ship class was imported from ship file above)
+        #this allows us to access Ship's methods within the Alien Invasion class
+        #The (self) input arguement refers to the current instance of AlienInvasion and gives Ship access to the game's resouces, e.g. screen
+        #A good way to remember this is that if a method needs anything from THIS LEVEL (i.e. the screen attribute) then pass (self)
+        #I.e. - SHIP NEEDS TO ACCESS DATA WITHIN ALIENINVASION
+
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True: #This loop will run continually 
+            #This 'event' loop will 'listen' for user inputs during game
+            # Watch for keyboard and mouse events.
+
+            self._check_events() #Starting with a '_' indicates a HELPER METHOD (will be defined outside of this method, in its own method below)
+            #^Call this method via SELF._check_events
+
+            self.ship.update() #The ships position will be updated after we have checked for keyboard events
+
+            # Redraw the screen during each pass through the loop.
+            self._update_screen()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip() #Will continually update display to show most recent positions of game 
+
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get(): #This function returns a list of events that have taken place since the last time this function was called
+            if event.type == pygame.QUIT: #Any keyboard or mouse events will cause this loop to run
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN: #If key pressed DOWN
+                if event.key == pygame.K_RIGHT: #if right key
+                    # Move the ship to the right.
+                    self.ship.moving_right = True #change the flag to true in the instance of ship
+            elif event.type == pygame.KEYUP: #if key is UP
+                if event.key == pygame.K_RIGHT: #if it is the right key
+                    self.ship.moving_right = False #reset flag to false 
+
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to new screen"""
+        #self.bg color was defined in __init__
+        #As per notes, can acccess all self. methods within a class
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+
+
+if __name__ == '__main__': #when a file is ran directly, its __name__ variable is set to __main__
+    #when a file is imported as a MODULE, its __name__ is set to the file's name
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ Project1_AlienInvasion/ship.py
@@ -0,0 +1,35 @@
+import pygame
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+
+        #ai_game input gives Ship access to all the game resources defined in AlienInvasion
+        #(AlienInvasion is a class defined in alien_invasion.py)
+        self.screen = ai_game.screen #Accessing the screen attribute of the AlienInvasion class
+        #^This allows us to easily access it with the methods of this class
+        self.screen_rect = ai_game.screen.get_rect() #Accessing the screen's rect attribute, allowing us to place the ship at the correct position
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('Project1_AlienInvasion/images/ship.bmp')
+        self.rect = self.image.get_rect() #Getting the ship's 'rectangle' values so we can better position it 
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom #Assigning a variable of the middle bottom of the ship as the middle bottom of the screen 
+
+        #Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect) #'Draws' the ship at the position determined
+        #'blit' essentially means 'draw on top of'
+        #blit is an inbuilt method of the Pygame library in Python
