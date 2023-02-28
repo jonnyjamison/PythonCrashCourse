7d1b42a7cb1e6f190da5551b491910bd7db7fac2
jonnyjamison <jonny.jamison@outlook.com>
2023-02-28 20:42:53 +0000

    Added test_employee

--- Chapter11_Testing_Code/test_employee.py
+++ Chapter11_Testing_Code/test_employee.py
@@ -7,12 +7,12 @@ class TestEmployee(unittest.TestCase):
 
     def setUp(self):
         """Make an employee to use in tests."""
-        self.eric = Employee('eric', 'matthes', 65000)
+        self.eric = Employee('eric', 'matthes', 65000) #This saves us from having to create a new instance for each test 
 
     def test_give_default_raise(self):
         """Test that a default raise works correctly."""
         self.eric.give_raise()
-        self.assertEqual(self.eric.salary, 70000)
+        self.assertEqual(self.eric.salary, 70000) #asserting that values are equal 
 
     def test_give_custom_raise(self):
         """Test that a custom raise works correctly."""
